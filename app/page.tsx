7826147db79c3aa5b2f3bfe961aecfa1e0d6dcaa
Omar Ahmed Abdel Hameed <omar.ahmed.nes6@gmail.com>
2026-02-11 03:05:37 +0200

    Fetch Surah data from API and display using React state and effect

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,39 @@
-export default function Home() {
-  return (
-      <div className="flex min-h-screen items-center justify-center">
-        <h1 className="text-4xl font-bold">Hello World! 🚀</h1>
-      </div>
-  );
-}
+"use client";
+// @ts-ignore
+import {useEffect, useState} from "react";
+
 interface Surah{
-    surah_number: number;
-    juz: number;
-    total_ayat: number;
-    completed_ayat: number;
+    number: number
+    name: string
+    englishName: string
+    englishNameTranslation:string
+    numberOfAyahs: number
+    revelationType: string
+    completed_ayat?: number;
+}
+
+export default function Home() {
+    // 1. Memory: Create a "State" variable to hold the list.
+    // It starts as an empty array [].
+    const [surahs, setSurahs] = useState<Surah[]>([]);
+
+    // 2. The Trigger: "When this component starts..."
+    useEffect(() => {
+
+        // 3. The Worker: We define a function to go get the data.
+        const fetchMyData = async () => {
+            const res = await fetch("https://api.alquran.cloud/v1/surah");
+            const json = await res.json();
+
+            // 4. The Update: We take the array from 'json.data'
+            // and put it into our 'surahs' memory.
+            setSurahs(json.data);
+        };
+
+        // 5. Run the worker!
+       console.log( fetchMyData())
+
+    }, []); // 6. The "Only Once" rule.
+
+
 }
